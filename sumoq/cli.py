fc5f09a0c6967513c7705c2b037e26c58045daf5
Chen Chao Shih (Frank) <frankc@netskope.com>
2024-12-09 19:19:27 +0800

    Suggestions after "where". Copy to clipboard.

--- sumoq/cli.py
+++ sumoq/cli.py
@@ -1,4 +1,5 @@
 import asyncio
+import pyperclip
 import base64
 import os
 import re
@@ -10,27 +11,41 @@ import yaml
 from prompt_toolkit import PromptSession
 from prompt_toolkit.completion import Completer, Completion, FuzzyCompleter
 from prompt_toolkit.patch_stdout import patch_stdout
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 
 _indexes = []
-_cwd = os.path.abspath(__file__)
+_cwd = os.path.abspath(os.path.dirname(__file__))
 _encode = "utf-8"
 _custom_fields = []
 _namespaces = []
+_json_suggestions = {}
 _sumo_api_base = "https://api.sumologic.com/api/v1"
 
 
 class CompletionMode(Enum):
     FIELD = 1
     VALUE = 2
+    WHERE_VALUE = 3
 
 
 class SumoQueryCompleter(Completer):
-    _re_fields = r"^(([\w\.]+|%\".+\")=([\w\-:/\.+@#\$%\^]+|\".+\")\s+)*"
+    _re_field_unit = r"([\w\.]+|%\".+\")"
+    _re_value_unit = r"([\w\-:/\.+@#\$%\^]+|\".*\")"
+    _re_fields = rf"^({_re_field_unit}={_re_value_unit}\s+)*"
+    _re_where_fields = (
+        rf".*\|\s*where\s+({_re_field_unit}={_re_value_unit}\s+"
+        r"((and|AND|or|OR|not|NOT)\s+)+)*"
+    )
     RULES = (
+        (_re_fields + r"$", "field"),
         (_re_fields + r"_index=$", "index"),
         (_re_fields + r"_sourceName=$", "src_name"),
         (_re_fields + r"_loglevel=$", "log_level"),
-        (_re_fields + r"$", "field"),
+        (_re_where_fields + r"$", "where_field"),
+        (
+            _re_where_fields + r"(?P<where_field>([\w\.]+|%\".+\"))=$",
+            "where_value",
+        ),
     )
     COMPILED_RULES = [(re.compile(r[0]), r[1]) for r in RULES]
     FIELD_SPECIAL_CHAR_CHECK = re.compile(r"(^[\d\.]|.*[\W\.]|.*\.\.)")
@@ -58,7 +73,7 @@ class SumoQueryCompleter(Completer):
 
         cur_text = document.text_before_cursor
         for r, name in self.COMPILED_RULES:
-            if _ := re.match(r, cur_text):
+            if matched := re.match(r, cur_text):
                 if name == "index":
                     yield from self._yeild_completions(_indexes)
                 elif name == "src_name":
@@ -66,10 +81,21 @@ class SumoQueryCompleter(Completer):
                 elif name == "log_level":
                     yield from self._yeild_completions(self.LOG_LEVELS)
                 elif name == "field":
-                    # This should be checked later than the field values.
                     yield from self._yeild_completions(
                         self.BUILT_IN_FIELDS + _custom_fields,
                         mode=CompletionMode.FIELD,
+                    )
+                elif name == "where_field":
+                    yield from self._yeild_completions(
+                        _json_suggestions.keys(), mode=CompletionMode.FIELD
+                    )
+                elif name == "where_value":
+                    field = matched.group("where_field")
+                    if field.startswith('%"'):
+                        field = field.lstrip('%"').rstrip('"')
+                    yield from self._yeild_completions(
+                        _json_suggestions.get(field) or [],
+                        mode=CompletionMode.WHERE_VALUE,
                     )
                 break
 
@@ -82,6 +108,11 @@ class SumoQueryCompleter(Completer):
             elif mode is CompletionMode.VALUE:
                 if re.match(self.VALUE_SPECIAL_CHAR_CHECK, v):
                     v = f'"{v}"'
+            elif mode is CompletionMode.WHERE_VALUE:
+                if isinstance(v, str):
+                    v = f'"{v}"'
+                elif isinstance(v, bool):
+                    v = f'"{v}"'.lower()
 
             yield Completion(v)
 
@@ -117,16 +148,23 @@ async def cli(conf, keys, kubeconf):
     asyncio.create_task(fetch_custom_fields(session))
     asyncio.create_task(fetch_idx(session))
     asyncio.create_task(fetch_namespaces(kubeconf))
+    asyncio.create_task(fetch_json_suggestions(conf))
 
     try:
         p_session = PromptSession()
         with patch_stdout():
             result = await p_session.prompt_async(
-                "Query:",
+                "query> ",
                 multiline=True,
                 completer=FuzzyCompleter(SumoQueryCompleter()),
+                auto_suggest=AutoSuggestFromHistory(),
+                bottom_toolbar=("<tab> select a completion. "
+                "<esc> <enter> to exit.")
             )
-        print(result)
+        asyncclick.echo(result)
+
+        pyperclip.copy(result)
+        asyncclick.echo("\nCopied to clipboard")
     finally:
         await session.close()
 
@@ -203,9 +241,21 @@ async def fetch_namespaces(kubeconf):
         return
 
     _namespaces.extend(
-        line.split(" ", 1)[0] for line in
-        stdout.decode(_encode).split("\n")[1:]
+        line.split(" ", 1)[0]
+        for line in stdout.decode(_encode).split("\n")[1:]
     )
+
+
+async def fetch_json_suggestions(conf):
+    global _cwd, _json_suggestions
+
+    if not conf:
+        conf = os.path.join(_cwd, "default-config.yml")
+
+    with open(conf, "r") as cfp:
+        conf_dict = yaml.load(cfp, Loader=yaml.SafeLoader)
+
+    _json_suggestions = conf_dict.get("app_json") or {}
 
 
 if __name__ == "__main__":
